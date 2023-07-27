a539e8cb27c1a3782fea4118d6871eae91108bdd
snewman-aa <snewman.aa@gmail.com>
2023-07-27 11:28:39 -0400

    added metadata

--- metadata.py
+++ metadata.py
@@ -24,32 +24,21 @@ def appmetadata() -> AppMetadata:
     # first set up some basic information
     metadata = AppMetadata(
         name="Pix2struct Slates",
-        description="",  # briefly describe what the purpose and features of the app
-        app_license="",  # short name for a software license like MIT, Apache2, GPL, etc.
-        identifier="pix2struct-slates",  # should be a single string without whitespaces. If you don't intent to publish this app to the CLAMS app-directory, please use a full IRI format. 
-        url="https://fakegithub.com/some/repository",  # a website where the source code and full documentation of the app is hosted
+        description="Run Pix2Struct slate queries on input timeframes",
+        app_license="MIT",
+        identifier="pix2struct-slates",
+        url="https://github.com/clamsproject/app-pix2struct-slates",
         # (if you are on the CLAMS team, this MUST be "https://github.com/clamsproject/app-pix2struct-slates"
         # (see ``.github/README.md`` file in this directory for the reason)
-        analyzer_version='version_X', # use this IF THIS APP IS A WRAPPER of an existing computational analysis algorithm
-        # (it is very important to pinpoint the primary analyzer version for reproducibility)
-        # (for example, when the app's implementation uses ``torch``, it doesn't make the app a "torch-wrapper")
-        # (but, when the app doesn't implementaion any additional algorithms/model/architecture, but simply use API's of existing, for exmaple, OCR software, it is a wrapper)
-        # if the analyzer is a python app, and it's specified in the requirements.txt
-        # this trick can also be useful (replace ANALYZER_NAME with the pypi dist name)
-        analyzer_version=[l.strip().rsplit('==')[-1] for l in open('requirements.txt').readlines() if re.match(r'^ANALYZER_NAME==', l)][0],
-        analyzer_license="",  # short name for a software license
+        analyzer_version='1',
+        analyzer_license="Apache-2.0",
     )
     # and then add I/O specifications: an app must have at least one input and one output
-    metadata.add_input(DocumentTypes.Document)
-    metadata.add_output(AnnotationTypes.Thing, typeSpecificProperty='property-value')
-    
-    # (optional) and finally add runtime parameter specifications
-    metadata.add_parameter(name='a_param', description='example parameter description',
-                           type='boolean', default='false')
-    # metadta.add_parameter(more...)
-    
-    # CHANGE this line and make sure return the compiled `metadata` instance
-    return None
+    metadata.add_input(DocumentTypes.VideoDocument)
+    metadata.add_output(DocumentTypes.TextDocument)
+    metadata.add_output(AnnotationTypes.Alignment)
+
+    return metadata
 
 
 # DO NOT CHANGE the main block
